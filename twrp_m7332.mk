ad317384c809d620a71794a3462b1abf97194774
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-08 01:19:51 +0300

    Update twrp_m7332.mk

--- twrp_m7332.mk
+++ twrp_m7332.mk
@@ -9,9 +9,9 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_DEVICE := m7332
-PRODUCT_NAME := twrp_m7332
+PRODUCT_NAME := androidTV
 PRODUCT_BRAND := MTK
-PRODUCT_MODEL := Smart TV MT9632
+PRODUCT_MODEL := Smart tv 
 PRODUCT_MANUFACTURER := mstar
 
 # Force Dynamic Partitions Support (For GSI Flashing)
