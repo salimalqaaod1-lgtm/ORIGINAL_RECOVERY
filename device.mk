070674c464a6b717c8a0315b06e92cf807bd5684
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-08 00:42:05 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,6 +3,11 @@ LOCAL_PATH := .
 # Sideband for Remote Control
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ir_remote.ini:recovery/root/vendor/etc/ir_remote.ini
+
 # Support for Dynamic Partitions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:recovery/root/first_stage_ramdisk/fstab.m7332
+
+# Boot Script (تفعيل ملف التشغيل الذي يحتوي على أوامر الـ IR والـ USB)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/init.recovery.m7332.rc:recovery/root/init.recovery.m7332.rc
