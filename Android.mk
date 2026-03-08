e2e2eb92480e7cab6c93ae7d8104787e6a0d469e
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-09 00:50:37 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,9 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
+# تعريف مكتبة البوت كملف جاهز
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.boot@1.0
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_SRC_FILES := recovery/root/lib/android.hardware.boot@1.0.so
 LOCAL_MODULE_TAGS := optional
+# تأكد أن المسار أدناه يطابق مكان الملف في مستودعك بالضبط
+LOCAL_SRC_FILES := recovery/root/lib/android.hardware.boot@1.0.so
 LOCAL_MODULE_SUFFIX := .so
 include $(PREBUILT_SHARED_LIBRARY)
+
