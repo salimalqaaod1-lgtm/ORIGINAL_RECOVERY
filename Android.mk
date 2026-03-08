d63ef65d11c3fdeea00f998a3fbefa06f593b47f
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-09 00:52:41 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE := android.hardware.boot@1.0
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS := optional
 # تأكد أن المسار أدناه يطابق مكان الملف في مستودعك بالضبط
-LOCAL_SRC_FILES := recovery/root/lib/android.hardware.boot@1.0.so
+LOCAL_SRC_FILES := recovery/root/system/lib/android.hardware.boot@1.0.so
 LOCAL_MODULE_SUFFIX := .so
 include $(PREBUILT_SHARED_LIBRARY)
 
