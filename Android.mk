3752ca52f64b4c5b0485c6fc78063430b1b6b5b2
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-09 00:55:54 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,12 +1,20 @@
 LOCAL_PATH := $(call my-dir)
 
-# تعريف مكتبة البوت كملف جاهز
-include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.boot@1.0
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-# تأكد أن المسار أدناه يطابق مكان الملف في مستودعك بالضبط
-LOCAL_SRC_FILES := recovery/root/system/lib/android.hardware.boot@1.0.so
-LOCAL_MODULE_SUFFIX := .so
-include $(PREBUILT_SHARED_LIBRARY)
+# وظيفة لتسجيل المكتبات الجاهزة تلقائياً
+define add-prebuilt-lib
+    include $(CLEAR_VARS)
+    LOCAL_MODULE := $(1)
+    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+    LOCAL_MODULE_SUFFIX := .so
+    LOCAL_SRC_FILES := recovery/root/system/lib/$(1).so
+    LOCAL_MODULE_TAGS := optional
+    include $(PREBUILT_SHARED_LIBRARY)
+endef
 
+# تسجيل المكتبات التي ظهرت في صورك ويحتاجها الريكفري بشدة
+$(eval $(call add-prebuilt-lib,android.hardware.boot@1.0))
+$(eval $(call add-prebuilt-lib,android.hardware.boot@1.1))
+$(eval $(call add-prebuilt-lib,libc))
+$(eval $(call add-prebuilt-lib,libbase))
+$(eval $(call add-prebuilt-lib,libcutils))
+$(eval $(call add-prebuilt-lib,liblog))
