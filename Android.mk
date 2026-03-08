1953c2979a7cd7c5f42d581dbcf0b19dbc1e2e99
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-09 01:53:56 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,20 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.boot@1.0
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-# تأكد من المسار بناءً على صورك
-LOCAL_SRC_FILES := recovery/root/system/lib/android.hardware.boot@1.0.so
-LOCAL_CHECK_ELF_FILES := false
-LOCAL_MODULE_TAGS := optional
-include $(PREBUILT_SHARED_LIBRARY)
+define add-prebuilt-lib
+    include $(CLEAR_VARS)
+    LOCAL_MODULE := $(1)
+    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+    LOCAL_MODULE_SUFFIX := .so
+    LOCAL_SRC_FILES := recovery/root/system/lib/$(1).so
+    LOCAL_CHECK_ELF_FILES := false
+    LOCAL_MODULE_TAGS := optional
+    include $(PREBUILT_SHARED_LIBRARY)
+endef
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.boot@1.1
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES := recovery/root/system/lib/android.hardware.boot@1.1.so
-LOCAL_CHECK_ELF_FILES := false
-LOCAL_MODULE_TAGS := optional
-include $(PREBUILT_SHARED_LIBRARY)
+# مكتبات النظام الأساسية التي تظهر في صورك
+$(eval $(call add-prebuilt-lib,android.hardware.boot@1.0))
+$(eval $(call add-prebuilt-lib,android.hardware.boot@1.1))
+$(eval $(call add-prebuilt-lib,android.hardware.health@2.0-impl-default))
+$(eval $(call add-prebuilt-lib,libc))
+$(eval $(call add-prebuilt-lib,libbase))
+$(eval $(call add-prebuilt-lib,libcutils))
+$(eval $(call add-prebuilt-lib,liblog))
+$(eval $(call add-prebuilt-lib,libhidlbase))
+$(eval $(call add-prebuilt-lib,libutils))
+$(eval $(call add-prebuilt-lib,libcrypto))
+$(eval $(call add-prebuilt-lib,libm))
+$(eval $(call add-prebuilt-lib,libz))
+
