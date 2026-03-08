f89b5d1f67bfc3f35a80df003ac64e51e27bd303
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-09 01:32:00 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,30 +1,20 @@
 LOCAL_PATH := $(call my-dir)
 
-# وظيفة لتسجيل المكتبات الجاهزة تلقائياً
-define add-prebuilt-lib
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := $(1)
-    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-    LOCAL_MODULE_SUFFIX := .so
-    LOCAL_SRC_FILES := recovery/root/system/lib/$(1).so
-    LOCAL_MODULE_TAGS := optional
-    include $(PREBUILT_SHARED_LIBRARY)
-endef
-# تعريف ملفات الـ HW الجاهزة
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.health@2.0-impl-default
+LOCAL_MODULE := android.hardware.boot@1.0
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_SUFFIX := .so
-# تأكد من هذا المسار بدقة من صورك
-LOCAL_SRC_FILES := recovery/root/system/lib/hw/android.hardware.health@2.0-impl-default.so
+# تأكد من المسار بناءً على صورك
+LOCAL_SRC_FILES := recovery/root/system/lib/android.hardware.boot@1.0.so
+LOCAL_CHECK_ELF_FILES := false
 LOCAL_MODULE_TAGS := optional
 include $(PREBUILT_SHARED_LIBRARY)
 
-# تسجيل المكتبات التي ظهرت في صورك ويحتاجها الريكفري بشدة
-$(eval $(call add-prebuilt-lib,android.hardware.boot@1.0))
-$(eval $(call add-prebuilt-lib,android.hardware.boot@1.1))
-$(eval $(call add-prebuilt-lib,libc))
-$(eval $(call add-prebuilt-lib,libbase))
-$(eval $(call add-prebuilt-lib,libcutils))
-$(eval $(call add-prebuilt-lib,liblog))
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.boot@1.1
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES := recovery/root/system/lib/android.hardware.boot@1.1.so
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_MODULE_TAGS := optional
+include $(PREBUILT_SHARED_LIBRARY)
