eafa907695963e77b1e478ed79a4fac6fb801d10
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-09 04:08:37 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,5 @@ ALLOW_MISSING_DEPENDENCIES := true
 BUILD_BROKEN_MISSING_PGO_STATS_ERROR := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
-# تعطيل الخدمات التي تسبب الأخطاء (مثل الصحة والسيارات)
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.none
+
 AB_OTA_UPDATER := false
