4d8a3d88a968e1e8cd5f4f46ab4120d44b2b83e2
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-10 07:28:30 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,28 +1,2 @@
 LOCAL_PATH := $(call my-dir)
-
-define add-prebuilt-lib
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := $(1)
-    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-    LOCAL_MODULE_SUFFIX := .so
-    LOCAL_SRC_FILES := recovery/root/system/lib/$(1).so
-    
-    LOCAL_MODULE_TAGS := optional
-    include $(PREBUILT_SHARED_LIBRARY)
-endef
-
-# مكتبات النظام الأساسية التي تظهر في صورك
-$(eval $(call add-prebuilt-lib,android.hardware.boot@1.0))
-$(eval $(call add-prebuilt-lib,android.hardware.boot@1.1))
-# $(eval $(call add-prebuilt-lib,android.hardware.health@2.0-impl-default))
-$(eval $(call add-prebuilt-lib,libc))
-$(eval $(call add-prebuilt-lib,libbase))
-$(eval $(call add-prebuilt-lib,libcutils))
-$(eval $(call add-prebuilt-lib,liblog))
-$(eval $(call add-prebuilt-lib,libhidlbase))
-$(eval $(call add-prebuilt-lib,libutils))
-$(eval $(call add-prebuilt-lib,libcrypto))
-$(eval $(call add-prebuilt-lib,libm))
-$(eval $(call add-prebuilt-lib,libz))
-
-LOCAL_CHECK_ELF_FILES := false
+include $(call all-subdir-makefiles)
